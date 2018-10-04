60a4a852d2e79a9778217feec392a16f40730992
Jan Jouke Tjalsma <j.j.tjalsma@gmail.com>
2018-10-04 11:54:43 +0200

    implement alma client with guzzle and monolog

---
+++ src/Service/AlmaClient.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use GuzzleHttp\ClientInterface;
+
+class AlmaClient
+{
+    private $logger;
+    private $client;
+    private $host;
+    private $apiKey;
+
+
+    public function __construct(LoggerInterface $logger, ClientInterface $client, $host, $apiKey)
+    {
+        $this->logger = $logger;
+        $this->client = $client;
+        $this->host = $host;
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @param $method
+     * @param $uri
+     * @param array $params
+     * @param null $entity
+     * @return mixed
+     */
+    public function request($method, $uri, $params = array(), $entity = null)
+    {
+        try {
+            // Build the URI
+            $uri = rtrim($this->host, '/').'/almaws/v1/'.ltrim($uri, '/');
+
+            // Add API key to params
+            $params['apikey'] = $this->apiKey;
+
+            // Request JSON format
+            $params['format'] = 'json';
+
+            // Build options array
+            $options = [ 'query' => array_filter($params) ];
+            if ($entity) {
+                $options['json'] = $entity;
+            }
+            $options['verify'] = false; // Disable SSL verification
+
+            // Send request
+            $response = $this->client->request($method, $uri, $options);
+
+            // return response
+            return json_decode($response->getBody(), true);
+        } catch (RequestException $e) {
+            $this->logger->critical('ALMAclient request exception', json_decode($e->getResponse()->getBody()), true);
+        } catch (ServerException $e) {
+            $this->logger->critical('ALMAclient server exception', json_decode($e->getResponse()->getBody()), true);
+        }
+    }
+}
